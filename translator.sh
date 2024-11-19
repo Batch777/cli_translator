3a91fbf8f94c3ab5d528e42a8e4dcae5a7ce5ca0
Henry Potter <henrypotter@HenrydeMac-mini.local>
2024-11-19 13:48:29 +0800

    重写了路径方便alias

--- translator.sh
+++ translator.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # 定义 Python 脚本路径
-PYTHON_SCRIPT="./yi_translator.py"
+SCRIPT_PATH="$(realpath "$0")"
+SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
+PYTHON_SCRIPT=$SCRIPT_DIR"/yi_translator.py"
 
 empty_lines=0
 input_lines=()
